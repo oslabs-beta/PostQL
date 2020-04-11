fe4da1ba60bce6d79973989c2e080bdbe4aa25aa
Joshua Pagan <joshuajpagan@gmail.com>
2020-04-11 10:44:41 -0400

    basic electron functionality with typescript, react, styling, es6, linters, webpack

---
+++ src/react.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const Index = () => {
+    return <div>Hello PostQL</div>;
+}
+
+ReactDOM.render(<Index />, document.getElementById('app'));
