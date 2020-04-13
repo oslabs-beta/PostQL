0133957742ed1f744c491e8c55605324274ab489
mariodandrea <mariodandrea14@yahoo.com>
2020-04-13 19:15:42 -0400

    Added select folder functionality, added funciton that parses through the folder and returns file path of apollo express server

--- src/SelectFolder.tsx
+++ src/SelectFolder.tsx
@@ -1,14 +1,67 @@
 import * as React from 'react';
+const { dialog } = require('electron').remote;
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
 
 const SelectFolder = () => {
   const clicked = () => {
-    console.log('hi');
+    dialog
+      .showOpenDialog({
+        properties: ['openDirectory'],
+      })
+      .then((projectPath) => {
+        function fileLoop(projectPath) {
+          fs.readdir(projectPath, (err, files) => {
+            if (err) {
+              console.error('Could not list the directory.', err);
+              process.exit(1);
+            }
+
+            // console.log(files);
+
+            for (let i = 0; i < files.length; i++) {
+              let filePath = path.join(projectPath, files[i]);
+
+              fs.stat(filePath, (error, stat) => {
+                if (error) {
+                  console.log(error);
+                  return;
+                }
+
+                if (stat.isFile()) {
+                  // console.log('one file here');
+                  fs.readFile(filePath, (error, data) => {
+                    if (error) {
+                      console.log(error);
+                      return;
+                    }
+
+                    if (data.includes('apollo-server-express')) {
+                      return filePath;
+                    }
+                  });
+                } else {
+                  fileLoop(filePath);
+                }
+              });
+            }
+          });
+
+          return 'file not found';
+        }
+
+        fileLoop(projectPath);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div>
       <button type="button" onClick={clicked}>
-        CLICK ME
+        Select Directory
       </button>
     </div>
   );
