d3025dbd198301399bfa4a71840b68c361e866e8
Jason Heinowitz <jasonheinowitz@yahoo.com>
2020-04-14 17:38:45 -0400

    Added styles

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
 
+import '../styles/application.scss';
 import PlaygroundDisplay from './PlaygroundDisplay';
 import Analytics from './Analytics';
 
