7cb02ea0bfb373fef8fcb555e738b8ca2b969cc1
mariodandrea <mariodandrea14@yahoo.com>
2020-04-16 15:22:13 -0400

    Implemented google-react-chart into graph component with hard coded data

--- client/src/components/Analytics/Graph.tsx
+++ client/src/components/Analytics/Graph.tsx
@@ -25,6 +25,37 @@ const Graph: FC<PropTypes> = ({ previousUrl }) => {
     <>
       <Link to={previousUrl}>Back</Link>
       <h2 className="D3title">D3 Chart</h2>
+      <Chart
+        width={'500px'}
+        height={'300px'}
+        chartType="BarChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ['Query/Resolver', ' ', 'Time in ns', ' ', 'Time in ns'],
+          ['Query', 0, 291258, 0, 0],
+          ['posts', 0, 28870, 0, 0],
+          ['title', 28870, 14052, 76392, 14052],
+          ['user', 42922, 9453, 76392, 8432],
+          ['id', 52375, 8431, 76392, 6642],
+          ['firstName', 60806, 8176, 76392, 7920],
+          ['lastName', 68982, 7410, 76392, 5621],
+        ]}
+        options={{
+          title: 'Query',
+          chartArea: { width: '50%' },
+          isStacked: true,
+          series: [{ color: 'transparent' }, {}, { color: 'transparent' }, {}],
+          hAxis: {
+            title: 'Time in Nanoseconds',
+            minValue: 0,
+          },
+          vAxis: {
+            title: 'Query/Resolver',
+          },
+        }}
+        // For tests
+        rootProps={{ 'data-testid': '3' }}
+      />
     </>
   );
 };
