20f0ca211c73ad99ec82f1a68dacc0d6f4ba604c
mariodandrea <mariodandrea14@yahoo.com>
2020-04-18 10:54:21 -0400

    Implemented google react chart with dummy data

--- client/src/components/Analytics/Instance/Graph.tsx
+++ client/src/components/Analytics/Instance/Graph.tsx
@@ -29,6 +29,40 @@ const Graph: FC<PropTypes> = ({ previousUrl }) => {
         <Link to={`${previousUrl}/${queryID}`}>
           <button type="button">Back</button>
         </Link>
+        <Chart
+          width={'500px'}
+          height={'300px'}
+          chartType="BarChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            ['Query/Resolver', ' ', 'Time in ns'],
+            ['Query', 0, 3838321],
+            ['post', 0, 356972],
+            ['id', 356972, 79810],
+            ['title', 436782, 15926],
+            ['body', 452708, 12026],
+          ]}
+          options={{
+            title: 'Query',
+            chartArea: { width: '50%' },
+            isStacked: true,
+            series: [
+              { color: 'transparent' },
+              {},
+              { color: 'transparent' },
+              {},
+            ],
+            hAxis: {
+              title: 'Time in Nanoseconds',
+              minValue: 0,
+            },
+            vAxis: {
+              title: 'Query/Resolver',
+            },
+          }}
+          // For tests
+          rootProps={{ 'data-testid': '3' }}
+        />
       </div>
     </div>
   );
