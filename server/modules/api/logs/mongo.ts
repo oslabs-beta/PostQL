5e13e7cedfc7c5c51a0e445849f8018b56c7c812
Jason Heinowitz <jasonheinowitz@yahoo.com>
2020-04-18 16:46:44 -0400

    removed console.log

--- server/modules/api/logs/mongo.ts
+++ server/modules/api/logs/mongo.ts
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 
 require('dotenv').config();
 
-console.log(process.env);
-
 mongoose.connect(process.env.LOGS_DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const queryMetricsSchema = new mongoose.Schema({
