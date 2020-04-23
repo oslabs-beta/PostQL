77f3bfc7d9380491dbb0d7d70ea456b07a2b82d7
David Zhang <davidzhang8828@gmail.com>
2020-04-23 10:00:52 -0400

    Adding skeleton to logs test. Also included eslint plugin for jest.

--- server/__tests__/logsTests.js
+++ server/__tests__/logsTests.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-undef */
 describe('basic', () => {
   it('logic test to pass TravisCI', () => {
     expect(1 + 1).toBe(2);
   })
 });
+
+
+// unit tests
+
+
+// integration tests
