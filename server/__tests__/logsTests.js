4543b2692c4289b9565fc70f0aa74eb5104c6ecf
David Zhang <davidzhang8828@gmail.com>
2020-04-23 22:27:27 -0400

    WIP logs tests

--- server/__tests__/logsTests.js
+++ server/__tests__/logsTests.js
@@ -1,12 +1,14 @@
-/* eslint-disable no-undef */
+// logic test for Travis CI
 describe('basic', () => {
   it('logic test to pass TravisCI', () => {
     expect(1 + 1).toBe(2);
-  })
+  });
 });
-
 
 // unit tests
 
 
 // integration tests
+
+
+// endpoint tests
