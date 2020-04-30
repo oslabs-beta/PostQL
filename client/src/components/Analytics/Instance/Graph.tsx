4361daae72a4701c28ec447ed85063bf65984e2f
Jae Jin Han <jjlisahan@gmail.com>
2020-04-29 23:36:40 -0400

    Title color changes

--- client/src/components/Analytics/Instance/Graph.tsx
+++ client/src/components/Analytics/Instance/Graph.tsx
@@ -108,7 +108,7 @@ const Graph: FC< GraphProps> = (props: any) => {
       : (
         <div>
           <div className="split">
-            <h2 className="Graphtitle">Gantt Chart:</h2>
+            <h2 className="Graphtitle">Gantt Chart</h2>
             <Chart
               width="1000px"
               height="300px"
